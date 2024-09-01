5bfb127be697f5789e8554ebbce090c9c80ad52a
Wong Nicholas <wongyiznicholas@gmail.com>
2024-09-01 11:08:29 +0800

    feat: step two and three

--- cwcc.py
+++ cwcc.py
@@ -1,15 +1,64 @@
 import argparse
 import os
 
+
 def countNumberOfBytes(text_file):
-    print(os.path.getsize(text_file))
+    """
+    Counts the number of bytes in a text_file
+
+    Args:
+        text_file (str): file to count the number of bytes
+
+    Returns:
+        void
+    """
+    print(os.path.getsize(text_file), text_file)
+
+
+def countNumberOfLines(text_file):
+    """
+    Counts the number of lines in a text_file
+
+    Args:
+        text_file (str): file to count the number of lines
+
+    Returns:
+        void
+    """
+    with open(text_file, "rb") as f:
+        num_lines = sum(1 for _ in f)
+    print(num_lines, text_file)
+
+
+def countNumberOfWords(text_file):
+    """
+    Counts the number of words in a text_file
+
+    Args:
+        text_file (str): file to count the number of words
+
+    Returns:
+        void
+    """
+    word_count = 0
+    with open(text_file, "r", encoding="utf8") as f:
+        for lines in f.readlines():
+            line = lines.rstrip()
+            words = line.split()
+            word_count += len(words)
+    print(word_count, text_file)
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Our version of a wc tool!')
-    parser.add_argument('-c', dest="wc", help='wc tool', type=str)
+    parser = argparse.ArgumentParser(description='Our version of a wc tool')
+    parser.add_argument('-c', help='Counts the number of bytes', type=str)
+    parser.add_argument('-l', help='Counts the number of lines', type=str)
+    parser.add_argument('-w', help='Counts the number of words', type=str)
 
     args = parser.parse_args()
-    text_file = args.wc
-    countNumberOfBytes(text_file)
-
-
+    if args.c:
+        countNumberOfBytes(args.c)
+    elif args.l:
+        countNumberOfLines(args.l)
+    elif args.w:
+        countNumberOfWords(args.w)
