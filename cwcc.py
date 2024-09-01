fdda28ea89c5a87c5c989e43bcfd510cb0f21ceb
Wong Nicholas <wongyiznicholas@gmail.com>
2024-09-01 12:50:11 +0800

    feat: complete the wc tool

--- cwcc.py
+++ cwcc.py
@@ -10,9 +10,9 @@ def countNumberOfBytes(text_file):
         text_file (str): file to count the number of bytes
 
     Returns:
-        void
+        int: number of bytes in the file
     """
-    print(os.path.getsize(text_file), text_file)
+    return os.path.getsize(text_file)
 
 
 def countNumberOfLines(text_file):
@@ -23,11 +23,11 @@ def countNumberOfLines(text_file):
         text_file (str): file to count the number of lines
 
     Returns:
-        void
+        int: number of lines in the file
     """
     with open(text_file, "rb") as f:
         num_lines = sum(1 for _ in f)
-    print(num_lines, text_file)
+    return num_lines
 
 
 def countNumberOfWords(text_file):
@@ -38,7 +38,7 @@ def countNumberOfWords(text_file):
         text_file (str): file to count the number of words
 
     Returns:
-        void
+        int: number of words in the file
     """
     word_count = 0
     with open(text_file, "r", encoding="utf8") as f:
@@ -46,7 +46,7 @@ def countNumberOfWords(text_file):
             line = lines.rstrip()
             words = line.split()
             word_count += len(words)
-    print(word_count, text_file)
+    return word_count
 
 
 def countNumberOfCharacters(text_file):
@@ -57,29 +57,36 @@ def countNumberOfCharacters(text_file):
         text_file (str): file to count the number of characters
 
     Returns:
-        void
+        int: number of characters in the file
     """
     character_count = 0
     with open(text_file, "r", newline='', encoding="utf8") as f:
         content = f.read()
         character_count = len(content)
-
-    print(character_count, text_file)
+    return character_count
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Our version of a wc tool')
-    parser.add_argument('-c', help='Counts the number of bytes', type=str)
-    parser.add_argument('-l', help='Counts the number of lines', type=str)
-    parser.add_argument('-w', help='Counts the number of words', type=str)
-    parser.add_argument('-m', help='Counts the number of characters', type=str)
+    parser.add_argument(
+        '-c', help='Counts the number of bytes', type=str, required=False)
+    parser.add_argument(
+        '-l', help='Counts the number of lines', type=str, required=False)
+    parser.add_argument(
+        '-w', help='Counts the number of words', type=str, required=False)
+    parser.add_argument(
+        '-m', help='Counts the number of characters', type=str, required=False)
+    parser.add_argument('filename', help='The file to analyze', type=str)
 
     args = parser.parse_args()
     if args.c:
-        countNumberOfBytes(args.c)
+        print(countNumberOfBytes(args.c), args.c)
     elif args.l:
-        countNumberOfLines(args.l)
+        print(countNumberOfLines(args.l), args.l)
     elif args.w:
-        countNumberOfWords(args.w)
+        print(countNumberOfWords(args.w), args.w)
     elif args.m:
-        countNumberOfCharacters(args.m)
+        print(countNumberOfCharacters(args.m), args.m)
+    elif args.filename:
+        filename = args.filename
+        print(f"{countNumberOfLines(filename)} {countNumberOfWords(filename)} {countNumberOfBytes(filename)} {filename}")
