f2b0852da276ffab9f2a6de3916ade8c09506eaf
Wong Nicholas <wongyiznicholas@gmail.com>
2024-09-08 11:21:45 +0800

    feat: change the design of the json parser function

--- 2-jsonparser/jsonParser.ts
+++ 2-jsonparser/jsonParser.ts
@@ -1,4 +1,35 @@
-export const parseJson = (jsonString: string): number => {
-  if (jsonString === "{}") return 0;
-  return 1;
+import { readFile } from "./tests/utils";
+
+/**
+ * Checks if the json provided is valid or not valid
+ * @param jsonString the json string
+ * @returns true if the json is valid, else false
+ */
+export const isJsonValid = (jsonString: string): boolean => {
+  if (jsonString.length === 0) return false;
+  const stack = [];
+
+  const splitJsonString = jsonString.split("\n");
+
+  for (const line of splitJsonString) {
+    const stripLine = line.trim();
+    for (const character of stripLine) {
+      if (character === "{") stack.push(character);
+      else if (character === "}") stack.pop();
+    }
+  }
+  return stack.length === 0;
+
+  // // only 1 line
+  // if (splitJsonString.length === 1) {
+  //   const regexOneLiner = new RegExp('\\s*{\\s*(".*"\\s*:\\s*".*")?\\s*}\\s*');
+  //   return regexOneLiner.test(jsonString);
+  // }
+
+  // const regex = new RegExp('{(".*":s?".*")?}');
+  // return regex.test(jsonString);
 };
+
+const string = "\n\n\n{}";
+
+console.log(isJsonValid(string));
--- 2-jsonparser/tests/jsonParser.test.ts
+++ 2-jsonparser/tests/jsonParser.test.ts
@@ -1,17 +1,35 @@
-import { parseJson } from "../jsonParser";
+import { isJsonValid } from "../jsonParser";
 import { readFile } from "./utils";
 import { test, expect, describe } from "@jest/globals";
 
-describe("step 1 tests", () => {
+describe("step1 tests", () => {
   const directory = "step1";
   test("parse empty json object", () => {
     const file = readFile(directory, "valid.json");
-    expect(parseJson(file)).toBe(0);
+    expect(isJsonValid(file)).toBe(true);
   });
 
   test("parse invalid json object", () => {
     const file = readFile(directory, "invalid.json");
-    expect(parseJson(file)).toBe(1);
+    expect(isJsonValid(file)).toBe(false);
+  });
+
+  test("parse a json object with empty strings behind and in front", () => {
+    const jsonEmptyStrings = "         {}         ";
+
+    expect(isJsonValid(jsonEmptyStrings)).toBe(true);
+  });
+
+  test("parse a json object with new lines in front and in behind and empty strings too", () => {
+    const jsonEmptyStrings = " \n   \n   \n         {     \n \n   }         ";
+
+    expect(isJsonValid(jsonEmptyStrings)).toBe(true);
+  });
+
+  test("parse a json object without a closing curly braces", () => {
+    const jsonEmptyStrings = " \n   \n   \n         {     \n \n            ";
+
+    expect(isJsonValid(jsonEmptyStrings)).toBe(false);
   });
 });
 
@@ -20,22 +38,22 @@ describe("step 1 tests", () => {
 
 //   test("parse a json object with additional comma", () => {
 //     const file = readFile(directory, "invalid.json");
-//     expect(parseJson(file)).toBe(1);
+//     expect(isJsonValid(file)).toBe(false);
 //   });
 
 //   test("parse a json object where the key has no quotes", () => {
 //     const file = readFile(directory, "invalid2.json");
-//     expect(parseJson(file)).toBe(1);
+//     expect(isJsonValid(file)).toBe(false);
 //   });
 
 //   test("parse a json object with 2 key-value pair", () => {
 //     const file = readFile(directory, "valid2.json");
-//     expect(parseJson(file)).toBe(0);
+//     expect(isJsonValid(file)).toBe(true);
 //   });
 
-//   test("parse a json object with 1 key-value pair", () => {
+//   test("parse a json object with false key-value pair", () => {
 //     const file = readFile(directory, "valid.json");
-//     expect(parseJson(file)).toBe(0);
+//     expect(isJsonValid(file)).toBe(true);
 //   });
 // });
 
@@ -44,12 +62,12 @@ describe("step 1 tests", () => {
 
 //   test("parse a json object with correct boolean syntax", () => {
 //     const file = readFile(directory, "valid.json");
-//     expect(parseJson(file)).toBe(0);
+//     expect(isJsonValid(file)).toBe(true);
 //   });
 
 //   test("parse a json object with incorrect boolean syntax", () => {
 //     const file = readFile(directory, "invalid.json");
-//     expect(parseJson(file)).toBe(1);
+//     expect(isJsonValid(file)).toBe(false);
 //   });
 // });
 
@@ -58,16 +76,16 @@ describe("step 1 tests", () => {
 
 //   test("parse a json with objects, arrays, strings and numbers for the value", () => {
 //     const file = readFile(directory, "valid.json");
-//     expect(parseJson(file)).toBe(0);
+//     expect(isJsonValid(file)).toBe(true);
 //   });
 
 //   test("parse a json object with an object with key-value pair inside and array also have values in it", () => {
 //     const file = readFile(directory, "valid2.json");
-//     expect(parseJson(file)).toBe(0);
+//     expect(isJsonValid(file)).toBe(true);
 //   });
 
 //   test("parse a json object with incorrect string syntax in the array object", () => {
 //     const file = readFile(directory, "invalid.json");
-//     expect(parseJson(file)).toBe(1);
+//     expect(isJsonValid(file)).toBe(false);
 //   });
 // });
