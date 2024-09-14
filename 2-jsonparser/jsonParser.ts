bb8d8824cfb94420bc78a5382cf26e9344b3bfac
Wong Nicholas <wongyiznicholas@gmail.com>
2024-09-14 18:10:14 +0800

    feat: step 3 complete!

--- 2-jsonparser/jsonParser.ts
+++ 2-jsonparser/jsonParser.ts
@@ -1,149 +1,197 @@
 import { readFile } from "./tests/utils";
 
-/**
- * Parse the string and return the number of characters to skip
- * @param string parse the string in the json object
- */
-const parseString = (jsonObject: string, index: number): number => {
-  index = parseSpaceAndNewlines(jsonObject, index);
-  let result = -1;
-  for (let i = index; i < jsonObject.length; i++) {
-    const character = jsonObject.charAt(i);
-    if (character === '"') {
-      result = i + 1;
-      break;
-    } else if (character === " ") {
-    } else {
-      break;
-    }
-  }
-  const value = jsonObject.substring(result).search('"') + result;
-  if (value === -1)
-    throw new Error(`Something wrong in the string of key-value pair!`);
+export class JsonParser {
+  private jsonString: string = "";
+  private index: number = 0;
 
-  return value + 1;
-};
-
-const parseSpaceAndNewlines = (jsonObject: string, index: number): number => {
-  for (let i = index; i < jsonObject.length; i++) {
-    const character = jsonObject.charAt(i);
-    if (character === " " || character === "\n") {
-    } else {
-      return i;
-    }
-  }
-};
-
-const parseGeneralCharacters = (
-  jsonObject: string,
-  index: number,
-  char: string
-): number => {
-  let result = -1;
-  for (let i = index; i < jsonObject.length; i++) {
-    const character = jsonObject.charAt(i);
-    if (character === char) {
-      result = i + 1;
-      break;
-    } else if (character === " " || character === "\n") {
-    } else {
-      console.log();
-      throw new Error(
-        `Found a weird character ${character} at index of ${index} while parsing ${char}`
-      );
-    }
-  }
-  if (result === -1) throw new Error(`Couldn't find the character ${char}`);
-  return result;
-};
-
-const parseGeneralCharactersWithoutThrowingError = (
-  jsonObject: string,
-  index: number,
-  char: string
-): number => {
-  let result = -1;
-  for (let i = index; i < jsonObject.length; i++) {
-    const character = jsonObject.charAt(i);
-    if (character === char) {
-      result = i + 1;
-      break;
-    } else if (character === " " || character === "\n") {
-    } else {
-      break;
-    }
-  }
-  return result;
-};
-
-const parseLeftover = (jsonObject: string, index: number): boolean => {
-  for (let i = index; i < jsonObject.length; i++) {
-    const character = jsonObject.charAt(i);
-    if (character !== " " && character !== "\n")
-      throw new Error(
-        `There's a leftover character ${character} with index ${i} after closing the curly brackets!`
-      );
-  }
-  return true;
-};
-
-/**
- * Checks if the json provided is valid or not valid
- * @param jsonString the json string
- * @returns true if the json is valid, else false
- */
-export const isJsonValid = (jsonString: string): boolean => {
-  const stripLine = jsonString.trim();
-  let result: number;
-
-  try {
-    result = parseGeneralCharacters(stripLine, 0, "{");
-    // attempt to parse close curly bracket
-    const parseCloseCurlyBracketResult =
-      parseGeneralCharactersWithoutThrowingError(stripLine, result, "}");
-
-    if (parseCloseCurlyBracketResult !== -1) {
-      // have to check remaining leftover
-      parseLeftover(stripLine, parseCloseCurlyBracketResult);
-    } else {
-      let commaResult = result;
-
-      // parse key-value pairs
-      while (commaResult !== -1) {
-        result = commaResult;
-        console.log(result);
-
-        // parsing keys
-        result = parseString(stripLine, result);
-
-        // continue parsing for the colon
-        result = parseGeneralCharacters(stripLine, result, ":");
-
-        // parsing the value
-        result = parseString(stripLine, result);
-
-        // parsing the comma
-        commaResult = parseGeneralCharactersWithoutThrowingError(
-          stripLine,
-          result,
-          ","
-        );
-      }
-
-      // parse closing curly bracket
-      const finalClosingCurlyBracket =
-        parseGeneralCharactersWithoutThrowingError(stripLine, result, "}");
-
-      if (finalClosingCurlyBracket !== -1) {
-        // have to check remaining leftover
-        parseLeftover(stripLine, finalClosingCurlyBracket);
+  /**
+   * Parse the string and return the number of characters to skip
+   * @param string parse the string in the json object
+   */
+  private parseString = () => {
+    // TODO: Can fix this
+    for (let i = this.index; i < this.jsonString.length; i++) {
+      const character = this.jsonString.charAt(i);
+      if (character === '"') {
+        this.index = i + 1;
+        break;
+      } else if (character === " ") {
       } else {
-        throw new Error("No curly bracket found!!");
+        break;
       }
     }
-    return true;
-  } catch (e) {
-    console.log(e.message);
-    return false;
-  }
-};
+    const value =
+      this.jsonString.substring(this.index).search('"') + this.index;
+    if (value === -1)
+      throw new Error(`Something wrong in the string of key-value pair!`);
+
+    this.index = value + 1;
+  };
+
+  private parseSpaceAndNewlines = () => {
+    for (let i = this.index; i < this.jsonString.length; i++) {
+      const character = this.jsonString.charAt(i);
+      if (character === " " || character === "\n") {
+      } else {
+        this.index = i;
+        break;
+      }
+    }
+  };
+
+  // parse value
+  private parseCharacters = (word: string) => {
+    for (let i = 0; i < word.length; i++) {
+      const character = word.charAt(i);
+      if (this.jsonString.charAt(i + this.index) !== character) {
+        throw new Error("parsing null error!");
+      }
+    }
+    this.index += word.length;
+  };
+
+  // parse number
+  private parseNumber = () => {
+    for (this.index; this.index < this.jsonString.length; this.index++) {
+      const character = this.jsonString.charAt(this.index);
+      if (character >= "0" && character <= "9") {
+      } else {
+        break;
+      }
+    }
+  };
+
+  private parseValue = () => {
+    this.parseSpaceAndNewlines();
+
+    const nextCharacter = this.jsonString.charAt(this.index);
+    const regexNumber = new RegExp("\\d");
+
+    // if the value is a string
+    if (nextCharacter === '"') {
+      this.parseString();
+    }
+
+    // if the value is a null
+    else if (nextCharacter === "n") {
+      this.parseCharacters("null");
+    }
+
+    // if the value is a boolean true
+    else if (nextCharacter === "t") {
+      this.parseCharacters("true");
+    }
+
+    // if the value is a false
+    else if (nextCharacter === "f") {
+      this.parseCharacters("false");
+    }
+
+    // if the value is a number
+    else if (regexNumber.test(nextCharacter)) {
+      this.parseNumber();
+    }
+
+    // other values that should not exist in a json value
+    else {
+      throw new Error(
+        `The value ${nextCharacter} at index of ${this.index} is not a valid format!`
+      );
+    }
+  };
+
+  private parseGeneralCharactersWithoutThrowingError = (
+    char: string
+  ): number => {
+    let result = -1;
+    for (let i = this.index; i < this.jsonString.length; i++) {
+      const character = this.jsonString.charAt(i);
+      if (character === char) {
+        result = i + 1;
+        break;
+      } else if (character === " " || character === "\n") {
+      } else {
+        break;
+      }
+    }
+    return result;
+  };
+
+  private parseLeftover = () => {
+    for (let i = this.index; i < this.jsonString.length; i++) {
+      const character = this.jsonString.charAt(i);
+      if (character !== " " && character !== "\n")
+        throw new Error(
+          `There's a leftover character ${character} with index ${i} after closing the curly brackets!`
+        );
+    }
+  };
+
+  /**
+   * Checks if the json provided is valid or not valid
+   * @param jsonString the json string
+   * @returns true if the json is valid, else false
+   */
+  isJsonValid = (jsonString: string): boolean => {
+    this.jsonString = jsonString.trim();
+
+    try {
+      this.parseSpaceAndNewlines();
+      this.parseCharacters("{");
+
+      // attempt to parse close curly bracket
+      let result = this.parseGeneralCharactersWithoutThrowingError("}");
+
+      if (result !== -1) {
+        this.index = result;
+        // have to check remaining leftover
+        this.parseLeftover();
+      } else {
+        let commaResult = this.index;
+
+        // parse key-value pairs
+        while (commaResult !== -1) {
+          this.index = commaResult;
+
+          // parsing keys
+          this.parseSpaceAndNewlines();
+          this.parseString();
+          // console.log(result, "1");
+
+          // continue parsing for the colon
+          this.parseSpaceAndNewlines();
+          this.parseCharacters(":");
+          // console.log(result, "2");
+
+          // parsing the value
+          this.parseValue();
+          // console.log(result, "3");
+
+          // parsing the comma
+          commaResult = this.parseGeneralCharactersWithoutThrowingError(",");
+        }
+        // parse closing curly bracket
+        this.index = this.parseGeneralCharactersWithoutThrowingError("}");
+
+        if (this.index !== -1) {
+          // have to check remaining leftover
+          this.parseLeftover();
+        } else {
+          throw new Error("No curly bracket found!!");
+        }
+      }
+      return true;
+    } catch (e) {
+      console.log(e.message);
+      return false;
+    }
+  };
+}
+
+const jsonParser = new JsonParser();
+
+// console.log(jsonParser.isJsonValid(`{\n"key": "value",\n"key2": "value"\n}`));
+// console.log(
+//   jsonParser.isJsonValid(" \n   \n   \n         {     \n \n   }         ")
+// );
+// console.log(jsonParser.isJsonValid(`{"key": "value"}`))
--- 2-jsonparser/tests/jsonParser.test.ts
+++ 2-jsonparser/tests/jsonParser.test.ts
@@ -1,115 +1,125 @@
-import { isJsonValid } from "../jsonParser";
 import { readFile } from "./utils";
 import { test, expect, describe } from "@jest/globals";
+import { JsonParser } from "../jsonParser";
 
-describe("step1 tests", () => {
-  const directory = "step1";
-  test("parse empty json object", () => {
-    const file = readFile(directory, "valid.json");
-    expect(isJsonValid(file)).toBe(true);
+describe("Test Cases for json parser", () => {
+  let jsonParser: JsonParser;
+
+  beforeEach(() => {
+    jsonParser = new JsonParser();
   });
 
-  test("parse invalid json object", () => {
-    const file = readFile(directory, "invalid.json");
-    expect(isJsonValid(file)).toBe(false);
+  describe("step1 tests", () => {
+    const directory = "step1";
+
+    test("parse empty json object", () => {
+      const file = readFile(directory, "valid.json");
+      expect(jsonParser.isJsonValid(file)).toBe(true);
+    });
+
+    test("parse invalid json object", () => {
+      const file = readFile(directory, "invalid.json");
+      expect(jsonParser.isJsonValid(file)).toBe(false);
+    });
+
+    test("parse a json object with empty strings behind and in front", () => {
+      const jsonEmptyStrings = "         {}         ";
+
+      expect(jsonParser.isJsonValid(jsonEmptyStrings)).toBe(true);
+    });
+
+    test("parse a json object with new lines in front and in behind and empty strings too", () => {
+      const jsonEmptyStrings = " \n   \n   \n         {     \n \n   }         ";
+
+      expect(jsonParser.isJsonValid(jsonEmptyStrings)).toBe(true);
+    });
+
+    test("parse a json object without a closing curly braces", () => {
+      const jsonEmptyStrings =
+        " \n   \n   \n         {     \n \n     **       ";
+
+      expect(jsonParser.isJsonValid(jsonEmptyStrings)).toBe(false);
+    });
   });
 
-  test("parse a json object with empty strings behind and in front", () => {
-    const jsonEmptyStrings = "         {}         ";
+  describe("step 2 tests", () => {
+    const directory = "step2";
 
-    expect(isJsonValid(jsonEmptyStrings)).toBe(true);
+    test("parse a json object with additional comma", () => {
+      const file = readFile(directory, "invalid.json");
+      expect(jsonParser.isJsonValid(file)).toBe(false);
+    });
+
+    test("parse a json object where the key has no quotes", () => {
+      const file = readFile(directory, "invalid2.json");
+      expect(jsonParser.isJsonValid(file)).toBe(false);
+    });
+
+    test("parse a json object with 2 key-value pair", () => {
+      const file = readFile(directory, "valid2.json");
+      expect(jsonParser.isJsonValid(file)).toBe(true);
+    });
+
+    test("parse a json object with a key-value pair", () => {
+      const file = readFile(directory, "valid.json");
+      expect(jsonParser.isJsonValid(file)).toBe(true);
+    });
+
+    test("parse a json object with key that has no quotes", () => {
+      expect(jsonParser.isJsonValid(`{key: "value"}`)).toBe(false);
+    });
+
+    test("parse a json object with value that has no quotes", () => {
+      expect(jsonParser.isJsonValid(`{"key": value}`)).toBe(false);
+    });
+
+    test("parse a json object with key that has 1 quote", () => {
+      expect(jsonParser.isJsonValid(`{key: "value"}`)).toBe(false);
+    });
+
+    test("parse a json object with value that has 1 quote", () => {
+      expect(jsonParser.isJsonValid(`{"key: "value"}`)).toBe(false);
+    });
+
+    test("parse a json object with no colon", () => {
+      expect(jsonParser.isJsonValid(`{"key "value"}`)).toBe(false);
+    });
+
+    test("parse a json object with no comma", () => {
+      expect(jsonParser.isJsonValid(`{"key: "value" "key2":"value"}`)).toBe(false);
+    });
   });
 
-  test("parse a json object with new lines in front and in behind and empty strings too", () => {
-    const jsonEmptyStrings = " \n   \n   \n         {     \n \n   }         ";
+  describe("step 3 tests", () => {
+    const directory = "step3";
 
-    expect(isJsonValid(jsonEmptyStrings)).toBe(true);
+    test("parse a json object with correct boolean syntax", () => {
+      const file = readFile(directory, "valid.json");
+      expect(jsonParser.isJsonValid(file)).toBe(true);
+    });
+
+    test("parse a json object with incorrect boolean syntax", () => {
+      const file = readFile(directory, "invalid.json");
+      expect(jsonParser.isJsonValid(file)).toBe(false);
+    });
   });
 
-  test("parse a json object without a closing curly braces", () => {
-    const jsonEmptyStrings = " \n   \n   \n         {     \n \n     **       ";
+  // describe("step 4 tests", () => {
+  //   const directory = "step4";
 
-    expect(isJsonValid(jsonEmptyStrings)).toBe(false);
-  });
+  //   test("parse a json with objects, arrays, strings and numbers for the value", () => {
+  //     const file = readFile(directory, "valid.json");
+  //     expect(jsonParser.isJsonValid(file)).toBe(true);
+  //   });
+
+  //   test("parse a json object with an object with key-value pair inside and array also have values in it", () => {
+  //     const file = readFile(directory, "valid2.json");
+  //     expect(jsonParser.isJsonValid(file)).toBe(true);
+  //   });
+
+  //   test("parse a json object with incorrect string syntax in the array object", () => {
+  //     const file = readFile(directory, "invalid.json");
+  //     expect(jsonParser.isJsonValid(file)).toBe(false);
+  //   });
+  // });
 });
-
-describe("step 2 tests", () => {
-  const directory = "step2";
-
-  test("parse a json object with additional comma", () => {
-    const file = readFile(directory, "invalid.json");
-    expect(isJsonValid(file)).toBe(false);
-  });
-
-  test("parse a json object where the key has no quotes", () => {
-    const file = readFile(directory, "invalid2.json");
-    expect(isJsonValid(file)).toBe(false);
-  });
-
-  test("parse a json object with 2 key-value pair", () => {
-    const file = readFile(directory, "valid2.json");
-    expect(isJsonValid(file)).toBe(true);
-  });
-
-  test("parse a json object with false key-value pair", () => {
-    const file = readFile(directory, "valid.json");
-    expect(isJsonValid(file)).toBe(true);
-  });
-
-  test("parse a json object with key that has no quotes", () => {
-    expect(isJsonValid(`{key: "value"}`)).toBe(false);
-  });
-
-  test("parse a json object with value that has no quotes", () => {
-    expect(isJsonValid(`{"key": value}`)).toBe(false);
-  });
-
-  test("parse a json object with key that has 1 quote", () => {
-    expect(isJsonValid(`{key: "value"}`)).toBe(false);
-  });
-
-  test("parse a json object with value that has 1 quote", () => {
-    expect(isJsonValid(`{"key: "value"}`)).toBe(false);
-  });
-
-  test("parse a json object with no colon", () => {
-    expect(isJsonValid(`{"key "value"}`)).toBe(false);
-  });
-
-  test("parse a json object with no comma", () => {
-    expect(isJsonValid(`{"key: "value" "key2":"value"}`)).toBe(false);
-  });
-});
-
-// describe("step 3 tests", () => {
-//   const directory = "step3";
-
-//   test("parse a json object with correct boolean syntax", () => {
-//     const file = readFile(directory, "valid.json");
-//     expect(isJsonValid(file)).toBe(true);
-//   });
-
-//   test("parse a json object with incorrect boolean syntax", () => {
-//     const file = readFile(directory, "invalid.json");
-//     expect(isJsonValid(file)).toBe(false);
-//   });
-// });
-
-// describe("step 4 tests", () => {
-//   const directory = "step4";
-
-//   test("parse a json with objects, arrays, strings and numbers for the value", () => {
-//     const file = readFile(directory, "valid.json");
-//     expect(isJsonValid(file)).toBe(true);
-//   });
-
-//   test("parse a json object with an object with key-value pair inside and array also have values in it", () => {
-//     const file = readFile(directory, "valid2.json");
-//     expect(isJsonValid(file)).toBe(true);
-//   });
-
-//   test("parse a json object with incorrect string syntax in the array object", () => {
-//     const file = readFile(directory, "invalid.json");
-//     expect(isJsonValid(file)).toBe(false);
-//   });
-// });
